a78e960a1f89f806b252f9f4acfb58eae763de92
Sonu-Munda1555 <117927176+Sonu-Munda1555@users.noreply.github.com>
2022-11-10 20:54:53 +0530

    Add files via upload

---
+++ CourseKotlinDev.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="icon.svg" type="image/x-icon">
+
+    <title>Learny Inc</title>
+
+    <!-- stylesheet -->
+    <link rel="stylesheet" href="./style/CoursesVideos.css">
+
+    <?php include './cdnLinks.php'; ?>
+
+</head>
+
+<body>
+    <?php include './components/Navbar.php'; ?>
+
+    <section class="course-wrapper">
+        <div class="container-xl">
+            <div class="video-wrapper">
+                <iframe src="https://www.youtube.com/embed/F9UC9DY-vIU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+
+            <div class="video-desc">
+
+                <div class="video-title">
+                    <h4 class="pt-3">Complete Mobile Development with Kotlin</h4>
+                </div>
+
+                <h6 class="py-1">Learn the Kotlin programming language in this introduction to Kotlin. Kotlin is a general purpose, open source, statically typed “pragmatic” programming language. It is used for many things, including Android development.</h6>
+
+                <div class="time-stamp">
+                    Lessons:
+
+                    <br>
+                    ⌨️ (0:00:50) Create Your First Kotlin Project
+                    <br>
+                    ⌨️ (0:04:23) Hello World
+                    <br>
+                    ⌨️ (0:06:33) Working With Variables
+                    <br>
+                    ⌨️ (0:11:04) Type System
+                    <br>
+                    ⌨️ (0:15:00) Basic Control Flow
+                    <br>
+                    ⌨️ (0:21:31) Basic Kotlin Functions
+                    <br>
+                    ⌨️ (0:27:12) Function Parameters
+                    <br>
+                    ⌨️ (0:32:52) Arrays
+                    <br>
+                    ⌨️ (0:35:28) Iterating with forEach
+                    <br>
+                    ⌨️ (0:41:17) Lists
+                    <br>
+                    ⌨️ (0:42:47) Maps
+                    <br>
+                    ⌨️ (0:45:05) Mutable vs Immutable Collections
+                    <br>
+                    ⌨️ (0:49:24) Vararg Parameters
+                    <br>
+                    ⌨️ (0:54:21) Named Arguments
+                    <br>
+                    ⌨️ (0:56:26) Default Parameter Values
+                    <br>
+                    ⌨️ (1:00:27) Create A Simple Class
+                    <br>
+                    ⌨️ (1:03:35) Adding Class Properties
+                    <br>
+                    ⌨️ (1:05:15) Class Init Block
+                    <br>
+                    ⌨️ (1:06:40) Accessing Class Properties
+                    <br>
+                    ⌨️ (1:07:32) Primary Constructor Properties
+                    <br>
+                    ⌨️ (1:08:17) Secondary Constructors
+                    <br>
+                    ⌨️ (1:09:50) Working With Multiple Init Blocks
+                    <br>
+                    ⌨️ (1:11:30) Default Property Values
+                    <br>
+                    ⌨️ (1:11:59) Properties With Custom Getters/Setters
+                    <br>
+                    ⌨️ (1:16:52) Class Methods
+                    <br>
+                    ⌨️ (1:20:12) Visibility Modifiers - Public/Private/Protected/Public
+                    <br>
+                    ⌨️ (1:22:30) Interfaces
+                    <br>
+                    ⌨️ (1:24:21) Abstract Classes
+                    <br>
+                    ⌨️ (1:26:13) Implementing An Interface
+                    <br>
+                    ⌨️ (1:26:35) Overriding Methods
+                    <br>
+                    ⌨️ (1:28:30) Default Interface Methods
+                    <br>
+                    ⌨️ (1:29:30) Interface Properties
+                    <br>
+                    ⌨️ (1:31:40) Implementing Multiple Interfaces
+                    <br>
+                    ⌨️ (1:32:57) Type Checking And Smart Casts
+                    <br>
+                    ⌨️ (1:36:18) Inheritance
+                    <br>
+                    ⌨️ (1:43:07) Object Expressions
+                    <br>
+                    ⌨️ (1:45:06) Companion Objects
+                    <br>
+                    ⌨️ (1:49:51) Object Declarations
+                    <br>
+                    ⌨️ (1:52:41) Enum Classes
+                    <br>
+                    ⌨️ (1:58:16) Sealed Classes
+                    <br>
+                    ⌨️ (2:00:07) Data Classes
+                    <br>
+                    ⌨️ (2:12:25) Extension Functions/Properties
+                    <br>
+                    ⌨️ (2:16:40) Higher-Order Functions
+                    <br>
+                    ⌨️ (2:29:07) Using The Kotlin Standard Library
+                    <br>
+                    ⌨️ (2:00:07) Data Classes
+                    <br>
+
+
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+    <?php include './components/Footer.php'; ?>
+</body>
+
+</html>
